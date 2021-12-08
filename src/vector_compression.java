850413df848726ce8dcf884bba8fbe27c8e00a4b
andrew <andrewnasief3.com>
2021-12-08 11:43:46 +0200

    added split & vecOfImg

--- src/vector_compression.java
+++ src/vector_compression.java
@@ -12,6 +12,8 @@ public class vector_compression {
     private int newWidth, newHeight;
     private int[][] imgVec;
     private Raster raster;
+    private Vector<int[][]> splited = new Vector<>();
+    public Vector<int[][]> mainSplit = new Vector<>(2);
     private int numOfVec;
 
     public void make_photo_vector() throws IOException {
@@ -19,6 +21,7 @@ public class vector_compression {
         width = img.getWidth();
         height = img.getHeight();
         imgVec = new int[width][height];
+
         raster = img.getData();
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
@@ -26,6 +29,7 @@ public class vector_compression {
             }
         }
     }
+
     public int calcVecSize() {
         int vecSize;
         vecSize = width * height;
@@ -37,7 +41,7 @@ public class vector_compression {
         newHeight += (height % 4) + height;
         System.out.println(newWidth + "aa" + width);
         System.out.println(newHeight);
-        numOfVec=(newHeight * newWidth) / 4;
+        numOfVec = (newHeight * newWidth) / 4;
     }
 
     public void mainVecAvg() {
@@ -45,46 +49,80 @@ public class vector_compression {
         float firElmAvr = 0, secElmAvr = 0, thirdElmAvr = 0, fourthElmAvr = 0;
         for (int i = 0; i < width; i += 2) {
             for (int j = 0; j < height; j += 2) {
-                    firElmAvr += imgVec[i][j];
+                firElmAvr += imgVec[i][j];
             }
 
         }
         System.out.println(firElmAvr);
-        mainVecAvr[0][0]=firElmAvr/numOfVec;
+        mainVecAvr[0][0] = firElmAvr / numOfVec;
         for (int i = 0; i < width; i += 2) {
             for (int j = 1; j < height; j += 2) {
-                    secElmAvr += imgVec[i][j];
+                secElmAvr += imgVec[i][j];
             }
         }
         System.out.println(secElmAvr);
-        mainVecAvr[0][1]=secElmAvr/numOfVec;
+        mainVecAvr[0][1] = secElmAvr / numOfVec;
         for (int i = 1; i < width; i += 2) {
             for (int j = 0; j < height; j += 2) {
-                    thirdElmAvr += imgVec[i][j];
+                thirdElmAvr += imgVec[i][j];
             }
         }
         System.out.println(thirdElmAvr);
-        mainVecAvr[1][0]=thirdElmAvr/numOfVec;
+        mainVecAvr[1][0] = thirdElmAvr / numOfVec;
         for (int i = 1; i < width; i += 2) {
             for (int j = 1; j < height; j += 2) {
-                    fourthElmAvr += imgVec[i][j];
+                fourthElmAvr += imgVec[i][j];
             }
         }
         System.out.println(fourthElmAvr);
-        mainVecAvr[1][1]=fourthElmAvr/numOfVec;
+        mainVecAvr[1][1] = fourthElmAvr / numOfVec;
     }
-    public void printMainAvrVec(){
-        for (int i = 0; i < 2; i ++) {
+
+    public void printMainAvrVec() {
+        for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                System.out.print(mainVecAvr[i][j]+" ");
+                System.out.print(mainVecAvr[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    public void makeMainSplit() {
+        int[][] firstVec = new int[2][2];
+        int[][] secVec = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                firstVec[i][j] = (int) mainVecAvr[i][j] - 1;
+                secVec[i][j] = (int) mainVecAvr[i][j] + 1;
+            }
+        }
+        mainSplit.add(firstVec);
+        mainSplit.add(secVec);
+    }
+
+    public void spilt() {
+        ;
+        for (int i = 0; i < newWidth; i += 2) {
+            for (int j = 0; j < newHeight; j += 2) {
+                int[][] book = new int[2][2];
+                for (int r = i; r < (i + 2); ++r) {
+                    for (int c = j; c < (j + 2); ++c) {
+                        if (r < width && c < height)
+                            book[r - i][c - j] = imgVec[r][c];
+                        else
+                            book[r - i][c - j] = 0;
+                    }
+                }
+                splited.add(book);
+            }
         }
     }
 
     public void print_imgVector() {
         System.out.println(height);
         System.out.println(width);
+        System.out.println(imgVec.length + " al length ya abo 3amo");
+        System.out.println(imgVec[0].length);
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 System.out.print(imgVec[i][j] + " ");
@@ -95,14 +133,34 @@ public class vector_compression {
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println("we are here");
-        vector_compression test =new vector_compression();
+        vector_compression test = new vector_compression();
         test.make_photo_vector();
         test.print_imgVector();
         test.CalSubVecNum();
         test.mainVecAvg();
-//        test.printMainAvrVec();
-//        int [][] x=new int[2][2];
+        test.makeMainSplit();
+        test.printMainAvrVec();
+        test.spilt();
+//        int[][] x=new int[5][6];
+//        x = test.mainSplit.get(1);
+//        for (int i = 0; i < 2; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.print(x[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 6; j++) {
+//                x[i][j]=2;
+//            }
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 6; j++) {
+//                x[i][j]=2;
+//            }
+//        }
+
+
 //        Vector<int[][]> v = new Vector<>(200);
 //        v.add(x);
 //        int [][]y=new int[2][2];
